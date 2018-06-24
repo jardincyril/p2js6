6bfa4812906c0fe126c567377c3421602d494971
cyril jardin <cyilroc60@hotmail.fr>
2018-06-24 13:11:42 +0200

    p2js6

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Changement d'image quand la souris passe sur l'image de base, l'image de base revient quand on enlève le curseur de l'image.
+function ImgChange(){
+  document.getElementById('imgs').src = 'assets/img/image1_2.jpg';
+  document.getElementById('imgs1').src = 'assets/img/image2_2.jpg';
+  document.getElementById('imgs2').src = 'assets/img/image3_2.jpg';
+  document.getElementById('imgs3').src = 'assets/img/image4_2.jpg';
+  document.getElementById('imgs5').src = 'assets/img/image5_2.jpg';
+}
